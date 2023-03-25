1e01f0799653a388b557bdce6cf66648801ae34a
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-03-25 22:08:05 +0530

    Hello C  World!!!

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!!!!!!!!! \n");
+    
+    printf("Hi! \n");
+    
+    printf("This is Pritu, I want to became your friend so I have decided to understand each and every aspect of your programming life.\n ");
+    
+    printf("so that we can help each other, growing together as good friend; agree na!! \n");
+    
+    printf("I know, you will be easy for me to understand because you start with letter 'c'; first word of 'cool' so you are so cool \n");
+    
+    printf("let's begin our sweet friendship.\n");
+    
+    printf("huuummm; you like semicolons very much\n");
+    
+    printf("okay, will meet you later, byee");
+    
+    return 0;
+}
